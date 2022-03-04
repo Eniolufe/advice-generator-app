10b1ded9eb9ad48359985f290d571f4653e33dc7
Ariwoola Ayooluwa <eniolufe@gmail.com>
2022-03-04 12:37:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const fetchAdvice = async () => {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+
+  document.getElementById("title").innerHTML = `Advice #${data.slip.id}`;
+  document.querySelector(".text").innerHTML = `"${data.slip.advice}"`;
+
+  console.log(data);
+};
+
+fetchAdvice();
